b8d9a6b295876d531197aec70ee0b58a8be29d9d
shallinta <740159135@qq.com>
2017-10-12 20:33:52 +0800

    chore: upgrade packing@2.4.0

--- config/webpack.build.babel.js
+++ config/webpack.build.babel.js
@@ -7,33 +7,13 @@
  * @param object appConfig config/packing.js中的配置
  */
 
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-
-export default (webpackConfig, program, appConfig) => {
+export default (webpackConfig) => {
   const config = webpackConfig;
 
   if (!config.resolve.alias) {
     config.resolve.alias = {};
   }
   config.resolve.alias.ajax = 'common/utils/ajax.js';
-
-  if (appConfig.cssModules) {
-    let cssModulesIdentName = '[path][name]__[local]--[hash:base64:5]';
-    if (appConfig.cssModulesIdentName) {
-      cssModulesIdentName = appConfig.cssModulesIdentName;
-    }
-    config.module.rules.forEach((rule) => {
-      if (rule.test.test('.css')) {
-        rule.loader = ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            { loader: 'css-loader', options: { importLoaders: 2, minimize: true, module: true, localIdentName: cssModulesIdentName } },
-            { loader: 'postcss-loader' }
-          ]
-        });
-      }
-    });
-  }
 
   return config;
 };
--- config/webpack.serve.babel.js
+++ config/webpack.serve.babel.js
@@ -7,29 +7,13 @@
  * @param object appConfig config/packing.js中的配置
  */
 
-export default (webpackConfig, program, appConfig) => {
+export default (webpackConfig) => {
   const config = webpackConfig;
 
   if (!config.resolve.alias) {
     config.resolve.alias = {};
   }
   config.resolve.alias.ajax = 'common/utils/ajax.js';
-
-  if (appConfig.cssModules) {
-    let cssModulesIdentName = '[path][name]__[local]--[hash:base64:5]';
-    if (appConfig.cssModulesIdentName) {
-      cssModulesIdentName = appConfig.cssModulesIdentName;
-    }
-    config.module.rules.forEach((rule) => {
-      if (rule.test.test('.css')) {
-        rule.use = [
-          { loader: 'style-loader' },
-          { loader: 'css-loader', options: { importLoaders: 2, module: true, localIdentName: cssModulesIdentName } },
-          { loader: 'postcss-loader' }
-        ];
-      }
-    });
-  }
 
   return config;
 };
